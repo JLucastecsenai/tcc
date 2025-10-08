91fb7f793f2cdf434092d236c41ada9342d877ae
Jlucastecsenai <joaolucasesiomega@gmail.com>
2025-10-08 20:51:10 -0300

    inicio da admin_homepage

--- restrito/admin_homepage.php
+++ restrito/admin_homepage.php
@@ -1,12 +1,28 @@
 <?php include "../validar.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>admin - restrito</title>
+    <link rel="stylesheet" href="admin_homepage.css">
 </head>
+
 <body>
-    <p>oi</p>
+    <header>
+        <div class="cabecalho">
+            <div class="logo">
+                <img src="../imgs/logo site.png" alt="logo do site">
+            </div>
+        </div>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
 </body>
+
 </html>
