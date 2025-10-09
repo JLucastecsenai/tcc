dd5cb66f78e977405767bf09917fc4175ad36e11
Jlucastecsenai <joaolucasesiomega@gmail.com>
2025-10-09 09:00:57 -0300

    sql e mais alterçoes

--- restrito/admin_homepage.php
+++ restrito/admin_homepage.php
@@ -4,6 +4,9 @@
 
 <head>
     <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>admin - restrito</title>
     <link rel="stylesheet" href="admin_homepage.css">
@@ -18,6 +21,23 @@
         </div>
     </header>
     <main>
+        <div class="MENSAGENS">
+            <h2>MENSAGENS</h2>
+            <p>Ao clicar no botão você sera recirecionado a uma pagina onde teram todas as mensagens de nossos clientes</p>
+            <button ></button>
+        </div>
+        <div class="CLIENTES">
+            <h2>CLIENTES</h2>
+        </div>
+        <div class="DEMANDAS">
+
+        </div>
+        <div class="FUNCIONARIOS">
+
+        </div>
+        <div class="LOGOUT">
+
+        </div>
 
     </main>
     <footer>
